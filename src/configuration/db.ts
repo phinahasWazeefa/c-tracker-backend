bff0f680b03d0ea06feee6e981bb0f7b03320b60
phinahasWazeefa <hello@ritzmotoclub.com>
2025-02-04 14:54:51 +0530

    db connection updated

--- src/configuration/db.ts
+++ src/configuration/db.ts
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 
 const uri = "mongodb+srv://ctracker:ctracker@my-cluster.udgav8g.mongodb.net/C-Tracker?retryWrites=true&w=majority&appName=My-Cluster";
 
-const MONGO_URI = uri || 'mongodb://localhost:27017/mydatabase';
-
+const MONGO_URI = uri ;
 const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_URI);
